8e55ae43bd062eed1df81da0c7af4e9f19dcd617
Benjamin Cherian <benjamin.cherian@3sharecorp.com>
2017-02-19 02:02:02 -0500

    Adding tags read form yml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filter :load_gtm
-  attr_accessor :containerId
+  before_filter :load_gtm, :load_tags
+  attr_accessor :containerId, :tags
 
   def index
   end
@@ -9,6 +9,11 @@ class ApplicationController < ActionController::Base
   def load_gtm
     config = YAML::load_file("config/gtm.yml")
     @containerId = config["containerId"]
+  end
+
+  def load_tags
+    config = YAML::load_file("config/tags.yml")
+    @tags = config["tags"]
   end
 
 
